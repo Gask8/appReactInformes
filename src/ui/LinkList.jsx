ce67176ca514730f360c8e4ec9a4b4c4b0fea2c0
Gask8 <ga.salandra@gmail.com>
2024-02-21 12:39:45 -0600

    adding links

--- src/ui/LinkList.jsx
+++ src/ui/LinkList.jsx
@@ -10,158 +10,200 @@ const Ul = styled.ul`
 
 function LinkList() {
   return (
-    <ul>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/territorio">
-          Territorio
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Acapulco">
-          Acapulco
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Campeche">
-          Campeche
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Cancún">
-          Cancún
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Chetumal">
-          Chetumal
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Costa Rica">
-          Costa
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Cotija">
-          Cotija
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Cozumel">
-          Cozumel
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Cuernavaca">
-          Cuernavaca
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Córdoba">
-          Córdoba
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/El Salvador">
-          El Salvador
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Guatemala">
-          Guatemala
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Lindavista">
-          Lindavista
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Morelia">
-          Morelia
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Mérida">
-          Mérida
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/México Norte">
-          México Norte
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/México Poniente">
-          México Poniente
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/México Sur">
-          México Sur
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Oaxaca">
-          Oaxaca
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Orizaba">
-          Orizaba
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Pachuca">
-          Pachuca
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Playa del Carmen">
-          Playa del Carmen
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Puebla">
-          Puebla
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Querétaro">
-          Querétaro
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Tapachula">
-          Tapachula
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Toluca">
-          Toluca
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Tuxtla Gutiérrez">
-          Tuxtla Gutiérrez
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Veracruz">
-          Veracruz
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Villahermosa">
-          Villahermosa
-        </a>
-      </li>
-      <li>
-        <a href="https://rcinformetrimestrales.netlify.app/analisis/Xalapa">
-          Xalapa
-        </a>
-      </li>
-    </ul>
+    <>
+      <ul>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/territorio">
+            Territorio
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Acapulco">
+            Acapulco
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Campeche">
+            Campeche
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Cancún">
+            Cancún
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Chetumal">
+            Chetumal
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Costa Rica">
+            Costa
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Cotija">
+            Cotija
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Cozumel">
+            Cozumel
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Cuernavaca">
+            Cuernavaca
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Córdoba">
+            Córdoba
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/El Salvador">
+            El Salvador
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Guatemala">
+            Guatemala
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Lindavista">
+            Lindavista
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Morelia">
+            Morelia
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Mérida">
+            Mérida
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/México Norte">
+            México Norte
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/México Poniente">
+            México Poniente
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/México Sur">
+            México Sur
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Oaxaca">
+            Oaxaca
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Orizaba">
+            Orizaba
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Pachuca">
+            Pachuca
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Playa del Carmen">
+            Playa del Carmen
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Puebla">
+            Puebla
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Querétaro">
+            Querétaro
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Tapachula">
+            Tapachula
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Toluca">
+            Toluca
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Tuxtla Gutiérrez">
+            Tuxtla Gutiérrez
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Veracruz">
+            Veracruz
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Villahermosa">
+            Villahermosa
+          </a>
+        </li>
+        <li>
+          <a href="https://rcinformetrimestrales.netlify.app/analisis/Xalapa">
+            Xalapa
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>https://rcinformetrimestrales.netlify.app/territorio</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Acapulco</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Campeche</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Cancún</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Chetumal</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Costa Rica</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Cotija</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Cozumel</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Cuernavaca</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Córdoba</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/El Salvador</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Guatemala</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Lindavista</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Morelia</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Mérida</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/México Norte</li>
+        <li>
+          https://rcinformetrimestrales.netlify.app/analisis/México Poniente
+        </li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/México Sur</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Oaxaca</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Orizaba</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Pachuca</li>
+        <li>
+          https://rcinformetrimestrales.netlify.app/analisis/Playa del Carmen
+        </li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Puebla</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Querétaro</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Tapachula</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Toluca</li>
+        <li>
+          https://rcinformetrimestrales.netlify.app/analisis/Tuxtla Gutiérrez
+        </li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Veracruz</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Villahermosa</li>
+        <li>https://rcinformetrimestrales.netlify.app/analisis/Xalapa</li>
+      </ul>
+      <div style={{ height: "50px" }}></div>
+    </>
   );
 }
 
